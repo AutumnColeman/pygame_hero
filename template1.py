0f366605015b8fcbf509b22b9d22a3002f5e9423
Autumn Coleman <autumn.eve.coleman@gmail.com>
2016-10-04 13:56:02 -0400

    Updated classes and keys

--- template1.py
+++ template1.py
@@ -1,27 +1,109 @@
-# Catch the Monster Game
-#
-# You will make a game in which the hero wins by catching the monster, while having to avoid the goblins. You will make use of object-oriented programming to make your life easier, but how and when will you use it? You will encounter a series of OOP Choice Points - where you can make the decision about whether to convert some code to OOP or not. Use your own judgement, there are no wrong choices.
-import pygame, random, time
+import pygame
+import time
+import random
 
-
+# KEY_UP = 273
+# KEY_DOWN = 274
+# KEY_LEFT = 276
+# KEY_RIGHT = 275
 
 class Monster(object):
-    def __init__(self, x, y):
-        self.img = pygame.image.load('images/monster.png').convert_alpha()
-        self.x = x
-        self.y = y
+    def __init__(self):
+        self.x = 100
+        self.y = 100
+        self.speed_x = 5
+        self.speed_y = 0
+        self.image = pygame.image.load('images/monster.png')
+        self.time_till_dir_change = None
+
+    def update(self, width, height):
+        self.x += self.speed_x
+        self.y += self.speed_y
+
+        if self.x > width:
+            self.x = 0
+        if self.y > height:
+            self.y = 0
+        if self.x < 0:
+            self.x = width
+        if self.y < 0:
+            self.y = height
+
+        self.maybe_change_direction()
+
+    def maybe_change_direction(self):
+        if self.time_till_dir_change is None:
+            now = time.time()
+            self.time_till_dir_change = now + 1
+            return
+
+        now = time.time()
+
+        if now >= self.time_till_dir_change:
+            self.time_till_dir_change = now + 1
+            # perform direction change
+            print 'perform direction change'
+            num = random.randint(0, 3)
+            if num == 0:
+                self.speed_x = 0
+                self.speed_y = -5
+            elif num == 1:
+                self.speed_x = 5
+                self.speed_y = 0
+            elif num == 2:
+                self.speed_x = 0
+                self.speed_y = 5
+            else:
+                self.speed_x = -5
+                self.speed_y = 0
+
+class Hero(object):
+    def __init__(self):
+        self.x = 250
+        self.y = 215
         self.speed_x = 2
         self.speed_y = 2
-#
-#
-#     # def update()
-#     def render(self):
-#         screen.blit(monster, (150, 150))
+        self.image = pygame.image.load('images/hero.png')
+        self.time_till_dir_change = None
+
+    def update(self, width, height):
+        self.x += self.speed_x
+        self.y += self.speed_y
+        # keeps hero within tree boundaries
+        if self.x > 450:
+            self.x = 450
+        if self.x < 30:
+            self.x = 30
+        if self.y > 415:
+            self.y = 415
+        if self.y < 30:
+            self.y = 30
+
+    def move(self, event):
+        if event.type == pygame.KEYDOWN:
+            print "key %r" % event.key # checks to see if key is hit
+            if event.key == pygame.K_UP:
+                self.speed_y -= 4
+            elif event.key == pygame.K_DOWN:
+                self.speed_y = 4
+            elif event.key == pygame.K_LEFT:
+                self.speed_x -= 4
+            elif event.key == pygame.K_RIGHT:
+                self.speed_x = 4
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_UP:
+                self.speed_y -= 0
+            elif event.key == pygame.K_DOWN:
+                self.speed_y = 0
+            elif event.key == pygame.K_LEFT:
+                self.speed_x -= 0
+            elif event.key == pygame.K_RIGHT:
+                self.speed_x = 0
 
 
 def main():
     # declare the size of the canvas
-    width = 510
+    width = 512
     height = 480
     blue_color = (97, 159, 182)
 
@@ -40,17 +122,17 @@ def main():
     ################################
     # PUT INITIALIZATION CODE HERE #
     ################################
-    #background image
-    background_image = pygame.image.load('images/background.png').convert_alpha()
-    #sprite characters
-    hero = pygame.image.load('images/hero.png').convert_alpha()
-    # monsterimg = pygame.image.load('images/monster.png').convert_alpha()
-    # monster_x = 50
-    # monster_y = 50
-    # monster_speed_x = 5
-    # monster_speed_y = 5
-    monster = Monster(30, 30)
-    goblin = pygame.image.load('images/goblin.png').convert_alpha()
+    background_image = pygame.image.load('images/background.png')
+    hero_image = pygame.image.load('images/hero.png')
+    hero = Hero()
+    # hero_x = 250
+    # hero_y = 200
+    # hero_speed_x = 3
+    # hero_speed_y = 0
+    monster = Monster()
+
+
+
     # game loop
     stop_game = False
     while not stop_game:
@@ -59,6 +141,28 @@ def main():
             ################################
             # PUT EVENT HANDLING CODE HERE #
             ################################
+            # moves the hero with arrow keys
+            # if event.type == pygame.KEYDOWN:
+            #     print "key %r" % event.key # checks to see if key is hit
+            #     if event.key == pygame.K_UP:
+            #         hero_speed_y -= 4
+            #     elif event.key == pygame.K_DOWN:
+            #         hero_speed_y = 4
+            #     elif event.key == pygame.K_LEFT:
+            #         hero_speed_x -= 4
+            #     elif event.key == pygame.K_RIGHT:
+            #         hero_speed_x = 4
+            # if event.type == pygame.KEYUP:
+            #     if event.key == pygame.K_UP:
+            #         hero_speed_y -= 0
+            #     elif event.key == pygame.K_DOWN:
+            #         hero_speed_y = 0
+            #     elif event.key == pygame.K_LEFT:
+            #         hero_speed_x -= 0
+            #     elif event.key == pygame.K_RIGHT:
+            #         hero_speed_x = 0
+            hero.move(event)
+
             if event.type == pygame.QUIT:
                 # if they closed the window, set stop_game to True
                 # to exit the main loop
@@ -66,42 +170,28 @@ def main():
 
         #######################################
         # PUT LOGIC TO UPDATE GAME STATE HERE #
-        ######################################
-        #moves the monster right and off screen
-
-        monster.x += monster.speed_x
-        monster.y += monster.speed_y
-        if monster.x + 60 > width:
-            monster.speed_x = -5
-        if monster.y + 60 > height:
-            monster.speed_y = -5
-        if monster.x - 30 < 0:
-            monster.speed_x = 3
-        if monster.y - 30 < 0:
-            monster.speed_y = 3
-
-        time_to_dir_change = time.time() + 2
-        now = time.time()
-        if now <= time_to_dir_change:
-            monster.x += 1
-            monster.y += 1
-            # monster_speed_x = 5
-            # monster_speed_y = 5
-            time_to_dir_change = time.time() + 2
-
-        # fill background color
-        screen.fill(blue_color)
+        #######################################
+        monster.update(width, height)
+        hero.update(width, height)
+        # hero_x += hero_speed_x
+        # hero_y += hero_speed_y
+        # # keeps hero within tree boundaries
+        # if hero_x > 450:
+        #     hero_x = 450
+        # if hero_x < 30:
+        #     hero_x = 30
+        # if hero_y > 415:
+        #     hero_y = 415
+        # if hero_y < 30:
+        #     hero_y = 30
 
         ################################
         # PUT CUSTOM DISPLAY CODE HERE #
         ################################
-        #renders background image
         screen.blit(background_image, (0, 0))
 
-        #renders sprites
-        screen.blit(hero, (250, 200))
-        screen.blit(monster.img, (monster.x, monster.y)),
-        screen.blit(goblin, (350, 350))
+        screen.blit(hero_image, (hero.x, hero.y))
+        screen.blit(monster.image, (monster.x, monster.y))
 
         # update the canvas display with the currently drawn frame
         pygame.display.update()
